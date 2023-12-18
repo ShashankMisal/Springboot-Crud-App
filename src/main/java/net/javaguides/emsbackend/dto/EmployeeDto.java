660c6c38ee4bf03f9b6936e0a33ddd5921afc161
shashank <shaank1818@gmail.com>
2023-12-19 00:18:56 +0530

    Initial commit

---
+++ src/main/java/net/javaguides/emsbackend/dto/EmployeeDto.java
@@ -0,0 +1,35 @@
+package net.javaguides.emsbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDto {
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
---
+++ src/main/java/net/javaguides/emsbackend/entity/Employee.java
@@ -0,0 +1,46 @@
+package net.javaguides.emsbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="employees")
+public class Employee {
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email_id",nullable = false,unique = true)
+    private String email;
+
+}
